29c411ede7fb490c1e6676737a4fc74390fa145e
Алексей <alexej.chernenko@gmail.com>
2016-07-12 18:34:11 +0300

    Переименование папки с лаунчером. Коррекция реадми (кодировку поменял, а то переводов кареток нет...)

--- Arcas/Launcer.cs
+++ Arcas/Launcer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Arcas
 {
-    public static class Launcer
+    public static class Program
     {
         public static void Main(string[] args)
         {
